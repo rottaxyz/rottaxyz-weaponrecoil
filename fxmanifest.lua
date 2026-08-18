b38445c4f34d1f028886d50ad5acbac1d16e8b8f
! Rotte_Kongen <168309535+rottaxyz@users.noreply.github.com>
2026-08-18 16:08:22 +0200

    chore: improve resource metadata

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'RottaK'
+description 'Lightweight, configurable weapon recoil reduction for FiveM.'
+version '1.1.0'
 
 shared_script 'config.lua'
 client_script 'client/client.lua'
